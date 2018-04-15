7dd6856d7c5ca15ff679ac81090667f267d9a342
MAC <mac@macde-macbook-pro-4.local>
2018-04-15 20:22:07 +0800

    筆電購物範例＋部分AutoLayout

--- ShoppingNotebook/ViewController.swift
+++ ShoppingNotebook/ViewController.swift
@@ -9,7 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var stepper_1: UIStepper!
+    @IBOutlet weak var stepper_2: UIStepper!
+    @IBOutlet weak var stepper_3: UIStepper!
+    @IBOutlet weak var stepper_4: UIStepper!
+    @IBOutlet weak var count_1: UILabel!
+    @IBOutlet weak var count_2: UILabel!
+    @IBOutlet weak var count_3: UILabel!
+    @IBOutlet weak var count_4: UILabel!
+    @IBOutlet weak var totalPrice: UILabel!
+    @IBAction func stepperValueChanged(_ sender: Any) {
+        let senderUI = sender as? UIStepper
+        if senderUI == stepper_1{
+            count_1.text = "\(stepper_1.value)"
+        }
+        else if senderUI == stepper_2{
+            count_2.text = "\(stepper_2.value)"
+        }
+        else if senderUI == stepper_3{
+            count_3.text = "\(stepper_3.value)"
+        }
+        else if senderUI == stepper_4{
+            count_4.text = "\(stepper_4.value)"
+        }
+        let data1 = stepper_1.value*20000+stepper_2.value*15000
+        let data2 = stepper_3.value*22000+stepper_4.value*17000
+        totalPrice.text = "\(data1+data2)"
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
